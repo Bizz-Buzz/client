5814eb3d6c3bfc345f0d7f1ce39410a75e5af53c
JasmineGWT <jazzgwwt@gmail.com>
2017-01-25 16:12:31 +1300

    Finished homepage until more seeded

--- components/viewBizz.js
+++ components/viewBizz.js
@@ -4,8 +4,8 @@ import ViewHandler from './bizzViews/viewHandler'
 module.exports = ({state, dispatch}) => {
   return (
     <div className="bizz">
-      <h1>{state.bizz.bizz_name}</h1>
-      <h4>{state.bizz.bizz_members} members</h4>
+      <h1 className="thing1">{state.bizz.bizz_name}</h1>
+      <h4 className="thing2">{state.bizz.bizz_members} members</h4>
       {ViewHandler(state,dispatch)}
     </div>
   )
