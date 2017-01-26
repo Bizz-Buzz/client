f19d4e239a2476bc762c109ecded85087215339b
JasmineGWT <jazzgwwt@gmail.com>
2017-01-27 10:06:12 +1300

    Added icons, trying to format them

--- components/header.js
+++ components/header.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-module.exports = (title, subtitle, backRoute, dispatch) => {
+module.exports = (welcome, name, backRoute, dispatch) => {
   function goBack() {
     dispatch({type: "CHANGE_ROUTE", payload: backRoute})
   }
   return (
     <div className="header">
-      <h1 className="greeting"><span><button onClick={()=>goBack()}>Back</button></span>{title}</h1>
-      <div><p className="greeting" id="greetingName">{subtitle}</p></div>
+      <span><button onClick={()=>goBack()}><img id="backIcon" src='https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-arrow-back-128.png' title="Back" /></button></span>
+      <div>
+      <p className="greeting">{welcome}</p>
+      <p className="greeting" id="greetingName">{name}</p>
+      </div>
     </div>
   )
 }
