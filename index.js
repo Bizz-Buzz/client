6caa2318d8c20bdcabed5e538174457bec1448cc
harrison-symes <symeshjb@gmail.com>
2017-01-27 06:59:11 +1300

    Updates

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import { createStore } from 'redux'
 import Login from './components/login'
 import Signup from './components/signup'
 import BizzList from './components/bizzList'
+import ViewBizz from './components/viewBizz'
+import FindBizz from './components/findBizz'
+import CreateBizz from './components/createBizz'
 
 var reducer = require('./reducer')
 
@@ -16,11 +19,17 @@ document.querySelector('main').appendChild(app)
 const initialState = {
   title: "Bizz-Buzz",
   route: '/',
+  isLoading: false,
   loginDetails: {},
   signupDetails: {},
   authError: null,
   user: {},
-  bizzList: []
+  bizzList: [],
+  allBizzList: [],
+  bizz: null,
+  buzzShow: false,
+  followRequests: [],
+  createBizzName: ''
 }
 
 var store = createStore(reducer, initialState)
@@ -30,7 +39,10 @@ const {getState, dispatch, subscribe} = store
 const route = Router({ default: '/404' }, [
   ['/', (params) => Login],
   ['/signUp', (params) => Signup],
-  ['/bizzList', (params) => BizzList]
+  ['/bizzList', (params) => BizzList],
+  ['/viewBizz', (params) => ViewBizz],
+  ['/findBizz', (params) => FindBizz],
+  ['/createBizz', (params) => CreateBizz]
 ])
 
 subscribe(() => {
